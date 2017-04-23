1401f7de9c2c430fbef1c02416f24716336df5eb
Norbert Wiener <PascalBock@Pascals-MBP.fritz.box>
2017-04-24 01:05:14 +0200

    Basic stuff

--- Matlab/ODE_System.m
+++ Matlab/ODE_System.m
@@ -1,151 +1,100 @@
-function dndt=ODE_System(t,n,p,oc)
+function dndt=ODE_System(t,nVec,p,oc)
 
-% special case: inlet
-p.nDotFeed=oc.mDot/sum(oc.x.*p.M) * oc.x;
+%% values needed for later computations
+% making vector to (p.Nxp.n)-Matrix
+nMat=reshape(nVec',p.n,p.N)';
+% sum over all species in one compartment
+nSum=sum(nMat,2)+oc.nAr;
+% Temperature
+T=(oc.p * p.V * p.epsilon)./( p.N * nSum * p.R); % [12]
+% molar fraction x for each compartment
+x=nMat./nSum;
 
-nMatrix=SolveLSE(p,oc,n);
-nGenMatrix=nRG(p,oc,n);
-% for both matrices:
-% row: 1:N : volume compartment
-% column: 1:3 : species
+%% Computing terms in ODE
+% initializing nMatrix and nGenMatrix
+nFlowMatrix=NaN(p.N,p.n); % molar stream between compartments
+nGenMatrix=NaN(p.N,p.n); % generated amount of species due to reactions
+
+% computing generated amount of species by reaction
+[nGenMatrix, r]=nGen(p,oc,T,x);
+
+% computing inlet/outlet stream of each compartment
+nFlowMatrix=nFlow(p,oc,nSum,T,x,sum(nGenMatrix,2),r);
+
+% calculating three ODEs (Ar calulated by 'closing condition': sum(x_j)=1)
+% for each compartment (x_j: mole fraction of species j)
+dndtMat=[oc.nDotFeed*oc.x(1:p.n); nFlowMatrix(1:end-1,:)]-nFlowMatrix+nGenMatrix;
 
 
+dndt=reshape(dndtMat',p.N*p.n,1); % making matrix to vector
 
-for i=1:p.N
-    for j=1:p.n
-        if i==1 % special case: inlet feed
-            dndtMat(i,j)=p.nDotFeed(j)-nMatrix(i,j)+nGenMatrix(i,j);
-        else % i>2
-            dndtMat(i,j)=nMatrix(i-1,j)-nMatrix(i,j)+nGenMatrix(i,j);
-        end % if
-    end % for j
-end % for i
-
-dndt=reshape(dndtMat',p.N*p.n,1);
-
-% if mod(t,0.1)==0
-%     disp(['Time t=',num2str(t)]);
-% end % if
 t
 end % function ODE_MassBalance
 
+function [nGen, r]=nGen(p,oc,T,x)
+%% calculating the produced/reacted amount of species
 
-function nDotrj=SolveLSE(p,oc,nVec)
-%% Calculation of Linear System of Equation b=A* \dot{n}^r
-b=NaN(p.N,1);
-A=zeros(p.N,p.N);
+%% some basics for further calulations
+% partial pressure pPart(j) by using molar fraction x for each compartment
+pPart=oc.p * x * 9.8692e-6; % CALCULATED IN !!!atm!!!
 
-% Calculating amount in volume compartment V_i in a vector, where row i is
-% the value for V_i
-nGen=sum(nRG(p,oc,nVec),2); % adding up rows
+%% reaction constant backward reaction
+% reaction constant for backwards reaction
+kMinus=p.k0*exp(-p.E./(p.R*T)); % [5]
 
-% making b vector
-n=nVec(1:p.n); % for first compartment
-T=TCalc(p,oc,n); % computing Temperature for first compartment (reactor inlet)
-Tfeed=TfeedCalc(p,oc,nVec); %v calculatung feed temperature
-Cp=sum(n)*p.cpg+p.Cp; % heat capacity of reactor compartment [22]
-b(1)=T*Cp*nGen(1) - p.deltaH*nGes(n)*r(p,oc,n)*p.mC + sum(p.nDotFeed) *...
-    (T*Cp + sum(p.nDotFeed)*p.cpg*nGes(n)*(Tfeed-T)); % [26]
-for i=2:p.N
-    n=nVec(p.n*i-(p.n-1):p.n*i); % computung n for compartment i
-    Cp=nGes(n)*p.cpg+p.Cp; % heat capacity of reactor compartment [22]
-    b(i)=TCalc(p,oc,n)*Cp*nGen(i) - p.deltaH*nGes(n)*r(p,oc,n)*p.mC; % [27]
-end % for
+%% reaction constant forward reaction
+% using Gillespie-Beattie coreelation
+alpha= 0.1191849./T + 91.87212./T.^2 + 25122730./T.^4; % [8]
+kGBStar= 10.^(-2.69112*log10(T) - 5.51926e-5*T +...
+    1.84886e-7*T.^2 + 2001.6./T + 2.6899); % [9]
+kGB=kGBStar.*10.^(alpha*p.pStar); % [7]
+Kp=kGB.^2;
+% finally: reaction constant for forward reaction
+kPlus=kMinus.*Kp; % [6]
 
-for i=1:p.N
-    n=nVec(p.n*i-(p.n-1):p.n*i); % amount of species in V_i
-    Cp=nGes(n)*p.cpg+p.Cp; % heat capacity of reactor compartment [22]
-    T=TCalc(p,oc,n); % Temperature at V_i
-    A(i,i)=T*Cp; % main diagonal of matrix [28]
-    if i>1
-        nMinus=nVec(p.n*(i-1)-(p.n-1):p.n*(i-1)); % amount of species in V_{i-1}
-        TMinus=TCalc(p,oc,nMinus); % Temperature at V_{i-1}
-        A(i,i-1)=-T*Cp - p.cpg*nGes(n) * (TMinus - T);  % second diagonal band of matrix [29]
-    end % if
-end % for
+%% rate of reaction by Temkin-Pyzhev equation
+r=p.f/p.rhoC * (kPlus  .* ( pPart(:,2).*pPart(:,1).^1.5 )./pPart(:,3) - ...
+    kMinus .*   pPart(:,3)                   ./pPart(:,1).^1.5); % [4]
 
-%% Solving LSE
-nDotr=NaN(p.N,1);
-nDotr=linsolve(A,b); % solving equation gives you amount of species at each volume
-% compartment
-% REMARK: maybe using OPT=UT: Upper triangular makes sense?
+% rate of genrating moles inside one reactor compartment
+nGen=p.eta.*r*p.mC*1/p.N; % [3]
 
-% getting value for each species by using mole fraction x
-nDotrj=NaN(p.N,p.n);
-for i=1:p.N
-    n=nVec(p.n*i-(p.n-1):p.n*i); % using n vector for each compartment
-    nDotrj(i,:)=nDotr(i)*x(n);
-end % for i
+end % function
 
-end
+function nFlow=nFlow(p,oc,nVec,T,x,nGenVec,r)
+
+%% Generating matrix A and vector b
+% computing heat capacity of reactor compartment Cp
+Cp=nVec*p.cpg + p.Cp; % [22]
+%% making vector b
+% computing Temperature at inlet of reactor (after heat exchanger heated up gas)
+Tinlet=TempInlet(p,oc,T);
+% first element: special case because inlet/feed
+bOne=Cp(1)*T(1)*nGenVec(1) - p.deltaH*p.mC*nVec(1)*r(1) + oc.nDotFeed *...
+    (T(1)*Cp(1) + p.cpg*nVec(1) * (Tinlet - T(1)) ); % [26]
+% rest of b
+b=T(2:end).*Cp(2:end).*nGenVec(2:end) - p.deltaH*p.mC*nVec(2:end).*r(2:end); % [27]
+% 'correcting' b by overwritting falsely computed first element
+b=[bOne; b];
+
+%% Making matrix A
+% writting entries on main diagonal
+A=diag(T.*Cp,0); % [28]
+% writting entries on first minor diagonal below
+a=-T(2:end).*Cp(2:end) + p.cpg*nVec(2:end) .* (T(1:end-1) - T(2:end)); % [29]
+A=A+diag(a,-1);
 
 
+%% Solving the Linear System of equation: A*nDot=b
+%Solving linear system of equation (A*nDot=b)
+nDot=linsolve(A,b); % options for linsolve (?maybe upper triangel?)
+
+% molar stream leaving compartment V_i/ comes from V_{i-1}
+nFlow=x.*nDot; % [30]
+end % function
 
 
-function nRG=nRG(p,oc,nVec)
-%% Calculating the generation term n^{r,g}
-for i=1:p.N % for all compartments
-    for j=1:p.n % for all three species
-        n=nVec(p.n*i-(p.n-1):p.n*i);
-        nRG(i,j)=p.eta(j)*r(p,oc,n)*p.mC*1/p.N; % computung the generated
-        % amount of each species j [3]
-    end % for j
-end % for i
-end % funtion CalcGenMoles
-
-function r=r(p,oc,n)
-%% Calculating reaction rate r
-
-% one time calculation of T
-T=TCalc(p,oc,n);
-
-% calculation of rate constant backwards reaction k-
-kMinus=p.k0* exp(- p.E/(p.R*T)); % [5]
-
-% Calculating dimensionless pressure
-pStar=oc.p/p.pAtmos; % [10]
-
-% using Gillespie-Beattie correlation
-alpha= 0.1191849/T + 91.87212/T^2 + 25122730/T^4; % [8]
-KpGBStar= 10^(-2.69112*log10(T) - 5.51926e-5*T + 1.84886e-7*T^2 + 2001.6/T + 2.6899); % [9]
-KpGB=KpGBStar*10^(alpha*pStar); % [7]
-
-% calculation of equilibrium constant
-Kp=KpGB^2; % [11]
-
-% calculation of rate konstant forward reaction k+
-kPlus=kMinus*Kp; % [6]
-
-% reaction rate by using Temkin-Pyzhev equation
-pPart=pPartCalc(oc,n)*9.8692e-6; % !!![atm]!!!
-r=p.f/p.rhoC * (kPlus * (pPart(2)*pPart(1)^1.5)/pPart(3) - kMinus * pPart(3)/pPart(1)^1.5 );
-end % function r
-
-function Tfeed=TfeedCalc(p,oc,nVec)
-%% Calculation the inlet temperature of reactor
-% Temperature at the end of reactor
-n=nVec(p.n*p.N-(p.n-1):p.n*p.N); % making vector of n for last compartment
-Tout=TCalc(p,oc,n); % computing temperature at reactor outlet
-% feed temperature after heat exchanger
-Tfeed=(oc.Tin + ( (p.U*p.A)/(oc.mDot*p.cpg) * Tout) ) / (1 + (p.U*p.A)/(oc.mDot*p.cpg) );
-end % function Tfeed
-
-function T=TCalc(p,oc,n)
-%% Calulating Temperature in one volume compartment
-T=(oc.p*p.V)/(nGes(n)*p.R*p.N) * p.epsilon; % [K] [12]
-end % function T
-
-function pPart=pPartCalc(oc,n)
-%% Calculating partial pressure
-pPart=x(n)*oc.p;
-end % function pPart
-
-function x=x(n)
-%% calculating mole fraction
-x=n./nGes(n);
-end % function x
-
-function nGes=nGes(n)
-%% calculating overall moles
-nGes=sum(n);
-end % function nGes
+function Tinlet=TempInlet(p,oc,T)
+% Temperatute at inlet of reactor (warmed up in heat exchanger)
+Tinlet=(oc.Tin + (p.U*p.A)/(oc.mDot*p.cpg) * T(p.N)) / (1 + (p.U*p.A)/(oc.mDot*p.cpg));
+end % function
---
+++ Matlab/make_Plots.m
@@ -0,0 +1,100 @@
+function make_Plots(p,oc,t,n)
+close all;
+%% figure1
+% plotting amuont of species
+figure(1)
+
+% interesting timePoint, which should get plotted
+timePoint=[1 5 p.timeStep];
+
+for i=1:3
+    % species to plot i=[1=H2, 2=N2, 3=NH3]
+    species=[i:p.n:p.N*p.n];
+    
+    % amount of  each species to different times stored in one matrix
+    plotMat=flipud(n(timePoint,species)'); % flipud so compartment 150 is up and 1 is down
+    % row: time
+    % column: compartment
+    
+    % compute minimum and maximum value
+    nMin=min(plotMat(:));
+    nMax=max(plotMat(:));
+    
+    % each species gets a new subplot
+    subplot(1,3,i)
+    plot(plotMat',[1:p.N],'+')
+    title(['species: ',p.speciesNames{i}])
+    % y-axis
+    yticks([1 25 50 75 100 125 150])
+    yticklabels({'150','125','100','75','50','25','1'})
+    ylabel('compartment [1]')
+    % x-axis
+    xticks([nMin mean([nMin,nMax]) nMax]);
+    xticklabels({[num2str(nMin)],[num2str(mean([nMin,nMax]))],[num2str(nMax)]})
+    xlabel('amount of species [kmol]')
+    % legend
+    legend([num2str(t(timePoint(1))),'h'],...
+        [num2str(t(timePoint(2))),'h'],...
+        [num2str(t(timePoint(3))),'h']);
+    
+end % for
+
+
+%% figure2
+% plotting temperature
+figure(2)
+% calculating temperature for each compartment at each time
+A=zeros(p.n*p.N,p.N);
+for i=1:p.N
+    A(p.n*(i-1)+1:p.n*i,i)=[1;1;1];
+end % for
+% sum over all species in one compartment
+nSum=n*A+oc.nAr;
+% Temperature
+T=(oc.p * p.V * p.epsilon)./( p.N * nSum * p.R); % [12]
+T=(T'); % transpose and switch upside down of T matrix
+imagesc(T); % plot
+colorbar; % show colorbar
+title('Temperature [K]')
+
+% y-axis
+yticks([1 25 50 75 100 125 150])
+yticklabels({'1','25','50','75','100','125','150'})
+ylabel('compartment [1]')
+% x-axis
+for i=1:p.timeStep
+labelxTicks{i}=[num2str(t(i)),'h'];
+end % for
+xticks([1:p.timeStep])
+xticklabels(labelxTicks)
+xlabel('time t [h]')
+
+
+
+%% figure3
+% plotting NH3 amount as heat map
+figure(3)
+nNH3=zeros(p.timeStep,p.N);
+for i=1:p.N
+nNH3(:,i)=n(:,3*i); % 
+end % for
+% row: point in time
+% column: compartment
+
+imagesc(nNH3'); % plot
+colorbar; % show colorbar
+title('amount of NH3 [kmol]')
+
+% y-axis
+yticks([1 25 50 75 100 125 150])
+yticklabels({'1','25','50','75','100','125','150'})
+ylabel('compartment [1]')
+% x-axis
+for i=1:p.timeStep
+labelxTicks{i}=[num2str(t(i)),'h'];
+end % for
+xticks([1:p.timeStep])
+xticklabels(labelxTicks)
+xlabel('time t [h]')
+
+end % function
